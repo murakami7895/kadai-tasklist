886eec613560ff8cb764d2291f3b5c4e0010720b
murakami <murakami7895@gmail.com>
2017-09-20 02:33:25 +0000

    add column corrected

--- database/migrations/2017_09_20_112820_add_status_tasks_table.php
+++ database/migrations/2017_09_20_112820_add_status_tasks_table.php
@@ -13,7 +13,7 @@ class AddStatusTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status');
+            $table->string('status', 10);
         });
     }
 
@@ -25,7 +25,7 @@ class AddStatusTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColum('status');
+            $table->dropColumn('status');
         });
     }
 }
