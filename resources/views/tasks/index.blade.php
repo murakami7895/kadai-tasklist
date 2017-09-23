bad31b674de0474166aa41e811c6c4537b0e9c82
murakami <murakami7895@gmail.com>
2017-09-23 12:34:10 +0000

    corrected

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,15 +9,15 @@
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>タイトル</th>
-                    <th>メッセージ</th>
+                    <th>ステータス</th>
+                    <th>タスク</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
                     <tr>
                         <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                        <td>{{ $task->title }}</td>
+                        <td>{{ $task->status }}</td>
                         <td>{{ $task->content }}</td>
                     </tr>
                 @endforeach
@@ -25,6 +25,6 @@
         </table>
     @endif
 
-    {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.create', '新規タスクの作成', null, ['class' => 'btn btn-primary']) !!}
 
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -7,22 +7,22 @@
     <table class="table table-bordered">
         <tr>
             <th>id</th>
-            <td>{{ $message->id }}</td>
+            <td>{{ $task->id }}</td>
         </tr>
         <tr>
-            <th>タイトル</th>
-            <td>{{ $message->title }}</td>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
         </tr>
         <tr>
-            <th>メッセージ</th>
-            <td>{{ $message->content }}</td>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
         </tr>
     </table>
     
-    {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
+    {!! link_to_route('tasks.edit', 'このタスクの編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
     
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-            {!! Form::submit('削除'), !!}
+            {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
 @endsection
