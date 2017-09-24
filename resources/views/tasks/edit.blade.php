d96c4689cef514a2ccf5215e326cd77db9a2349a
murakami <murakami7895@gmail.com>
2017-09-24 13:08:35 +0000

    edit-corrected

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -8,12 +8,12 @@
         <div class="col-xs-6">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 <div class="form-group">
-                    {!! Form::label('title', 'タイトル:') !!}
-                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('stasus', null, ['class' => 'form-control']) !!}
                 </div>
         
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
         
