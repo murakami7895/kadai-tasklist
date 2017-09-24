dbfb71acefc55a1cb03e32ebe6a5b3321b41d2ec
murakami <murakami7895@gmail.com>
2017-09-24 13:35:26 +0000

    edit-corected-3

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -9,7 +9,7 @@
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
-                    {!! Form::text('stasus', null, ['class' => 'form-control']) !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
         
                 <div class="form-group">
